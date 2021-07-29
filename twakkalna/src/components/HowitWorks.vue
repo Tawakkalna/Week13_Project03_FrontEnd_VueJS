f7585c6e0faf2660e9f69c7964df0be69f8dbde1
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-07-30 01:59:29 +0300

    video tutorials section + functionality

--- twakkalna/src/components/HowitWorks.vue
+++ twakkalna/src/components/HowitWorks.vue
@@ -8,78 +8,115 @@
 
     <div class="row">
       <div class="col-6 options">
-        <button type="button" class="acc-btns selected" @click="haveAcc">لدي حساب أبشر</button>
-        <button type="button" class="acc-btns" @click="dontHaveAcc">ليس لدي حساب أبشر</button>
+        <button type="button" class="acc-btns selected" @click="haveAcc">
+          لدي حساب أبشر
+        </button>
+        <button type="button" class="acc-btns" @click="dontHaveAcc">
+          ليس لدي حساب أبشر
+        </button>
         <button type="button" class="acc-btns">تسجيل الزائرين</button>
       </div>
       <div class="col-6">
-        <span id='noacc'>
-          إذا لم تكن مسجلًا في نظام أبشر، فإن تسجيلك في تطبيق توكلنا يتطلب <strong> تزكيتك من خلال أحد المسجلين </strong> في أبشر عبر خدمة تعريف رقم الجوال لتطبيق توكلنا المدرجة ضمن خدمات المنصة، حيث يمكن الوصول إليها عبر الطريقة التالية:
+        <span id="noacc">
+          إذا لم تكن مسجلًا في نظام أبشر، فإن تسجيلك في تطبيق توكلنا يتطلب
+          <strong> تزكيتك من خلال أحد المسجلين </strong> في أبشر عبر خدمة تعريف
+          رقم الجوال لتطبيق توكلنا المدرجة ضمن خدمات المنصة، حيث يمكن الوصول
+          إليها عبر الطريقة التالية:
         </span>
       </div>
     </div>
 
     <div class="row steps container">
-        <div class="col d-flex flex-column align-items-center"> 1 
-                <img class="arrow" src='../images/steps/step.png' alt='step1' />
-                <div class="step-image"> <img src='../images/steps/favicon.png' alt='step1' /> </div> 
-                <div class="step-text">حمل التطبيق</div>
-            </div>
-            <div class="col d-flex flex-column align-items-center">2 
-                <img class="arrow" src='../images/steps/step.png' alt='step1' />
-                <div class="step-image"> <img src='../images/steps/step2.png' alt='step1' /> </div> 
-                <div class="step-text">تسجيل مستخدم جديد وإدخال المعلومات المطلوبة</div>
-            </div>
-            <div class="col d-flex flex-column align-items-center">3 
-                <img class="arrow" src='../images/steps/step.png' alt='step1' />
-                <div class="step-image"> <img src='../images/steps/step3.png' alt='step1' /> </div> 
-                <div class="step-text">سيصلك رمز التحقق على جوالك المسجّل في أبشر لتفعيل الحساب</div>
-            </div>
-            <div class="col d-flex flex-column align-items-center">4 
-                <img class="arrow" src='../images/steps/step.png' alt='step1' />
-                <div class="step-image"> <img src='../images/steps/step4.png' alt='step1' /> </div> 
-                <div class="step-text">حدّد مكان إقامتك على الخريطة بشكل دقيق وأجب عن الأسئلة الصحية</div>
-            </div>
-            <div class="col d-flex flex-column align-items-center">5 
-                <div class="step-image"> <img src='../images/steps/step5.png' alt='step1' /> </div> 
-                <div class="step-text">حدد التابعين والمكفولين الموجودين في نفس مكان إقامتك (عائلة، عمال منزليون)</div>
-            </div>
+      <div class="col d-flex flex-column align-items-center">
+        1
+        <img class="arrow" src="../images/steps/step.png" alt="step1" />
+        <div class="step-image">
+          <img src="../images/steps/favicon.png" alt="step1" />
+        </div>
+        <div class="step-text">حمل التطبيق</div>
+      </div>
+      <div class="col d-flex flex-column align-items-center">
+        2
+        <img
+          class="arrow upsidedown"
+          src="../images/steps/step.png"
+          alt="step1"
+        />
+        <div class="step-image">
+          <img src="../images/steps/step2.png" alt="step1" />
+        </div>
+        <div class="step-text">تسجيل مستخدم جديد وإدخال المعلومات المطلوبة</div>
+      </div>
+      <div class="col d-flex flex-column align-items-center">
+        3
+        <img class="arrow" src="../images/steps/step.png" alt="step1" />
+        <div class="step-image">
+          <img src="../images/steps/step3.png" alt="step1" />
+        </div>
+        <div class="step-text">
+          سيصلك رمز التحقق على جوالك المسجّل في أبشر لتفعيل الحساب
+        </div>
+      </div>
+      <div class="col d-flex flex-column align-items-center">
+        4
+        <img
+          class="arrow upsidedown"
+          src="../images/steps/step.png"
+          alt="step1"
+        />
+        <div class="step-image">
+          <img src="../images/steps/step4.png" alt="step1" />
+        </div>
+        <div class="step-text">
+          حدّد مكان إقامتك على الخريطة بشكل دقيق وأجب عن الأسئلة الصحية
+        </div>
+      </div>
+      <div class="col d-flex flex-column align-items-center">
+        5
+        <div class="step-image">
+          <img src="../images/steps/step5.png" alt="step1" />
+        </div>
+        <div class="step-text">
+          حدد التابعين والمكفولين الموجودين في نفس مكان إقامتك (عائلة، عمال
+          منزليون)
+        </div>
+      </div>
 
+      <br />
+      <Tutorial />
     </div>
   </div>
 </template>
 <script>
-
-import $ from 'jquery';
+import $ from "jquery";
+import Tutorial from "./Tutorial.vue";
 
 export default {
-    name: "HowitWorks",
-    components: {
-
+  name: "HowitWorks",
+  components: {
+    Tutorial,
+  },
+  methods: {
+    dontHaveAcc() {
+      $("#noacc").show(333);
+      $(".selected").removeClass("selected");
+      $("button:nth-of-type(2)").addClass("selected");
     },
-    methods: {
-        dontHaveAcc(){
-            $('#noacc').show(333);
-            $(".selected").removeClass('selected');
-            $("button:nth-of-type(2)").addClass('selected');
-        },
-        haveAcc(){
-            $('#noacc').hide(333);
-            $(".selected").removeClass('selected');
-            $("button:nth-of-type(1)").addClass('selected');
-        }
+    haveAcc() {
+      $("#noacc").hide(333);
+      $(".selected").removeClass("selected");
+      $("button:nth-of-type(1)").addClass("selected");
     },
-    data(){
-        return{
-
-        }
-    },
-    mounted(){
-        $('#noacc').hide();
-    }
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    $("#noacc").hide();
+  },
 };
 </script>
-<style>
+<style scoped>
 .header {
   color: #1fa99b;
   text-align: center;
@@ -116,6 +153,10 @@ export default {
   height: 100px;
   border-radius: 50%;
   margin: 25px 0px;
+  transition: transform 0.5s;
+}
+.step-image:hover{
+  transform: rotateY(180deg);
 }
 .step-text {
   color: #3b444d;
@@ -130,13 +171,17 @@ export default {
 .arrow {
   height: 20px;
   top: 40px;
-  right: 175px;
+  right: 200px;
   position: absolute;
 }
 .steps div {
   position: relative;
 }
-#noacc{
-    font-size: 18px;
+#noacc {
+  font-size: 18px;
+}
+.upsidedown {
+  transform: rotate(180deg);
+  top: 120px;
 }
 </style>
